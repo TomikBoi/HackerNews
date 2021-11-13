435d460976736a4d3a06f1fb96c26234913786ef
TomikBoi <toms.kedo@gmail.com>
2021-11-13 11:17:40 +0200

    Added Loading component

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Loading from "./components/Loading";
 import Nav from "./components/Nav";
+Loading
 import "./index.css";
 
 class App extends React.Component {
@@ -10,6 +12,7 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav />
+          <Loading />
           <h1>Yo</h1>
         </div>
       </Router>
